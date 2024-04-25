fa3e10148b13579df3e716baf1f21e9c7ac4e5d5
agustrio1 <agustrio1998@gmail.com>
2024-04-25 10:13:11 +0700

    add limmiter

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,10 +2,17 @@ import express from "express";
 import { UserController } from "../controller/user.controller";
 import { validateToken } from "../middleware/auth";
 import { isAdmin } from "../middleware/isAdmin";
+import { rateLimit } from 'express-rate-limit'
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, 
+    max: 100,
+    message: 'Too many requests from this IP, please try again after an 15 minutes'
+})
 
 export const userRouter = express.Router();
 
-userRouter.get("/", validateToken, isAdmin, UserController.getAllUsers);
-userRouter.get("/:id", validateToken, UserController.getUserById);
-userRouter.put("/:id", validateToken, UserController.updateUser);
-userRouter.delete("/:id", validateToken, UserController.deleteUser);
+userRouter.get("/", limiter, validateToken, isAdmin, UserController.getAllUsers);
+userRouter.get("/:id", limiter, validateToken, UserController.getUserById);
+userRouter.put("/:id", limiter, validateToken, UserController.updateUser);
+userRouter.delete("/:id", limiter, validateToken, UserController.deleteUser);
