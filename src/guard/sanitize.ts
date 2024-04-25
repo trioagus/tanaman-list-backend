172f68d9cae1524a987b7ae001d27cd71c1e5990
agustrio1 <agustrio1998@gmail.com>
2024-04-25 09:34:34 +0700

    fix

--- src/guard/sanitize.ts
+++ src/guard/sanitize.ts
@@ -5,14 +5,19 @@
  * @returns {string} String input yang sudah dibersihkan.
  */
 export function sanitizeInput(input: string) {
+  // Mengganti satu tanda kutip dengan dua tanda kutip
   let sanitizedInput = input.replace(/'/g, "''");
 
+  // Menghilangkan tag HTML
   sanitizedInput = sanitizedInput.replace(/<[^>]*>/g, "");
+
+  // Menghilangkan potensi serangan XSS dan injection
   sanitizedInput = sanitizedInput.replace(
     /(\b)(on\S+)(\s*)=|javascript|(<\s*)(\/*)script/gi,
     ""
   );
 
+  // Menghapus spasi kosong di awal dan akhir string
   const trimmedInput = sanitizedInput.trim();
 
   return trimmedInput;
